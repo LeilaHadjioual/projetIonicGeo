264b90b9758350394b66d7bc96270b22fe9cbf91
unknown <leila.hadjioual@gmail.com>
2020-05-14 16:10:37 +0200

    add modal

--- src/app/site-details/site-details.page.ts
+++ src/app/site-details/site-details.page.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {DataUnescoService} from '../data-unesco.service';
 import {Observable} from 'rxjs';
+import {ModalController} from '@ionic/angular';
+import {ModalPage} from '../modal/modal.page';
 
 @Component({
     selector: 'app-site-details',
@@ -12,7 +14,7 @@ export class SiteDetailsPage implements OnInit {
     public siteDetails: string;
     information = null;
 
-    constructor(private activatedRoute: ActivatedRoute, private dataUnescoService: DataUnescoService, private router: Router) {
+    constructor(private activatedRoute: ActivatedRoute, private dataUnescoService: DataUnescoService, private modalController: ModalController) {
     }
 
     ngOnInit() {
@@ -22,10 +24,15 @@ export class SiteDetailsPage implements OnInit {
         });
     }
 
-    goToMap() {
-        console.log('hhh')
-        this.router.navigate(['/my-map']);
-
+    async openModal() {
+        const modal = await this.modalController.create({
+                component: ModalPage,
+                componentProps: {
+                    latitude: this.information.records[0].fields.coordinates[0],
+                    longitude: this.information.records[0].fields.coordinates[1]
+                }
+            });
+        await modal.present();
     }
 
 }
